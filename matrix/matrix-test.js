b6177919213e563ccfeb85db6c7ad1d6ac0b70b8
joelhoman <jhoman3@lion.lmu.edu>
2016-03-29 20:59:21 -0700

    added test cases, fixed matrix functions

--- matrix/matrix-test.js
+++ matrix/matrix-test.js
@@ -5,79 +5,59 @@ $(function () {
 
     // This suite checks instantiation basics.
     test("Creation and Data Access", function () {
-        var v = new Vector(5, 6, 3);
+        var m = new Matrix();
 
-        equal(v.dimensions(), 3, "Vector size");
-        equal(v.elements[0], 5, "First element by index");
-        equal(v.elements[1], 6, "Second element by index");
-        equal(v.elements[2], 3, "Third element by index");
-        equal(v.x(), 5, "First element by coordinate");
-        equal(v.y(), 6, "Second element by coordinate");
-        equal(v.z(), 3, "Third element by coordinate");
+        equal(m.height, 4, "Matrix height");
+        equal(m.width, 4, "Matrix width");
+        equal(m.totalElements(), 16, "Number of elements in matrix");
 
-        v = new Vector(300, 200);
+        var m2 = new Matrix([2, 3, 4, 5], 1, 4);
 
-        equal(v.dimensions(), 2, "Vector size");
-        equal(v.elements[0], 300, "First element by index");
-        equal(v.elements[1], 200, "Second element by index");
-        equal(v.x(), 300, "First element by coordinate");
-        equal(v.y(), 200, "Second element by coordinate");
-
-        v = new Vector(3, 2, 1, 2);
-
-        equal(v.dimensions(), 4, "Vector size");
-        equal(v.elements[0], 3, "First element by index");
-        equal(v.elements[1], 2, "Second element by index");
-        equal(v.elements[2], 1, "Third element by index");
-        equal(v.elements[3], 2, "Fourth element by index");
-        equal(v.x(), 3, "First element by coordinate");
-        equal(v.y(), 2, "Second element by coordinate");
-        equal(v.z(), 1, "Third element by coordinate");
-        equal(v.w(), 2, "Fourth element by coordinate");
-
-        v = new Vector();
-        equal(v.dimensions(), 0, "Empty vector (boundary case)");
+        equal(m2.height, 1, "Matrix height");
+        equal(m2.width, 4, "Matrix width");
+        equal(m2.totalElements(), 4, "Number of element in matrix");
     });
 
     test("Multiplication", function () {
-        var v = new Vector(8, 2, 3);
-        var vresult = v.multiply(2);
+        var m1 = new Matrix();
+        var result = [ 5, 0, 0, 0, 0, 5, 0, 0, 0, 0, 5, 0, 0, 0, 0, 5 ];
+        var m2 = new Matrix([ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 ], 4, 4);
+        equal(m2.multiply(m2).elements, result.elements, " Basic Multiplication");
 
-        equal(vresult.x(), 16, "Vector scalar multiplication first element");
-        equal(vresult.y(), 4, "Vector scalar multiplication second element");
-        equal(vresult.z(), 6, "Vector scalar multiplication third element");
-
-        vresult = vresult.divide(4);
-
-        equal(vresult.x(), 4, "Vector scalar division first element");
-        equal(vresult.y(), 1, "Vector scalar division second element");
-        equal(vresult.z(), 1.5, "Vector scalar division third element");
     });
 
-    test("Magnitude and Unit Vectors", function () {
-        var v = new Vector(3, 4);
-
-        // The classic example.
-        equal(v.magnitude(), 5, "2D magnitude check");
-
-        // Kind of a cheat, but still tests the third dimension.
-        v = new Vector(5, 0, 12);
-        equal(v.magnitude(), 13, "3D magnitude check");
-
-        // Now for unit vectors.
-        v = (new Vector(3, 4)).unit();
-
-        equal(v.magnitude(), 1, "2D unit vector check");
-        equal(v.x(), 3 / 5, "2D unit vector first element");
-        equal(v.y(), 4 / 5, "2D unit vector second element");
-
-        v = (new Vector(0, -7, 24)).unit();
-
-        equal(v.magnitude(), 1, "3D unit vector check");
-        equal(v.x(), 0, "3D unit vector first element");
-        equal(v.y(), -7 / 25, "3D unit vector second element");
-        equal(v.z(), 24 / 25, "3D unit vector third element");
+    test("Translate", function () {
+        var m1 = new Matrix();
+        var result = new Matrix([
+                                    1, 0, 0, 2,
+                                    0, 1, 0, 2,
+                                    0, 0, 1, 2,
+                                    0, 0, 0, 1
+                                ], 4, 4);
+        deepEqual(m1.translate(2, 2, 2).elements, result.elements, "Basic Translation");
     });
+
+    test("Scale", function () {
+        var m1 = new Matrix();
+        var result = new Matrix([
+                                    2, 0, 0, 0,
+                                    0, 2, 0, 0,
+                                    0, 0, 2, 0,
+                                    0, 0, 0, 1
+                                ], 4, 4);
+        deepEqual(m1.scale(2, 2, 2).elements, result.elements, "Basic Scale");
+    });
+
+    /*test("Rotate", function () {
+        var m1 = new Matrix();
+        var result = new Matrix([
+                                    ????
+                                    ????
+                                    ????
+                                    ????
+                                ], 4, 4);
+        deepEqual(m1.rotate(Math.PI / 2, 5, 5, 5).elements, result.elements, "Basic Rotate");
+    });*/
 
     test("Projection", function () {
         var v = new Vector(3, 3, 0);
@@ -97,5 +77,11 @@ $(function () {
             "Ensure that projection applies only to vectors with the same number of dimensions"
         );
     });
+    
+    /*test("Conversion", function () {
+        var m = new Matrix();
+        equal(m.convert(), [ 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 ], " Float32Array Conversion for WebGL");
+
+    });*/
 
 });
--- matrix/matrix.js
+++ matrix/matrix.js
@@ -1,107 +1,107 @@
 var Matrix = (function () {
     // Define the constructor.
-    var matrix = function () {
-        return [
-                    1, 0, 0, 0,
-                    0, 1, 0, 0,
-                    0, 0, 1, 0,
-                    0, 0, 0, 1
-                ];
+    var matrix = function (array, height, width) {
+        this.height = height || 4;
+        this.width = width || 4;
+        this.elements = array || [
+                                    1, 0, 0, 0,
+                                    0, 1, 0, 0,
+                                    0, 0, 1, 0,
+                                    0, 0, 0, 1
+                                ];
     };
 
-    matrix.prototype.dimensions = function () {
-        return 4;
+    matrix.prototype.height = function () {
+        return this.height;
     }
 
-    matrix.prototype.x = function () {
-        return this.elements[0];
+    matrix.prototype.width = function () {
+        return this.width;
     }
 
-    matrix.prototype.y = function () {
-        return this.elements[5];
-    }
-
-    matrix.prototype.z = function () {
-        return this.elements[10];
-    }
+    matrix.prototype.totalElements = function () {
+        return this.width * this.height;
+    };
 
     matrix.prototype.multiply = function (s) {
-        var result = new matrix();
+        var result = [];
 
-        for (var i = 0, max = this.dimensions(); i < max; i++) {
-            for (var j = 0; max = this.dimensions(); j < max; j++) {
-                var multiplier = [];
-                for (var k = 0; k < s.length; k++) {
-                    multiplier.push(multiplier[k][i]);
+        for (var i = 0, iMax = matrix.prototype.totalElements(); i < iMax; i += this.height) {
+            var row = [];
+            for (var e = i, eMax = e + this.width; e < eMax; e++) {
+                row.push(this.elements[ e ]);
+            }
+            for (var c = 0, cMax = s.width(); c < cMax; c++) {
+                var column = [];
+                for (var j = c, jMax = s.totalElements(); j < jMax; j += this.width) {
+                    column.push(s.elements[ j ]);
                 }
-                result.elements[i][j] = this.dot(multiplier, i);
+                result.push(matrix.prototype.dot(row, column));
             }
         }
-
         return result;
     };
 
-    matrix.prototype.dot = function (m, c) {
+    matrix.prototype.dot = function (row, column) {
         var result = 0;
 
-        for (var i = 0, max = this.dimensions(); i < max; i++) {
-            result += this.elements[c][i] * m.elements[i];
+        for (var i = 0, max = matrix.prototype.dimensions(); i < max; i++) {
+            result += row[i] * column[i];
         }
-
         return result;
     };
 
-    matrix.prototype.translation = function (tx, ty, tz) {
+    matrix.prototype.translate = function (tx, ty, tz) {
         var tx = tx || 0;
         var ty = ty || 0;
         var tz = tz || 0;
-        var t = [ 
-                    1, 0, 0, 0,
-                    0, 1, 0, 0,
-                    0, 0, 1, 0,
-                    tx, ty, tz, 1
-                 ];
-       return t;
-    }
+        var t = new Matrix([ 
+                                1, 0, 0, tx,
+                                0, 1, 0, ty,
+                                0, 0, 1, tz,
+                                0, 0, 0, 1
+                            ], 4, 4);
+        return t;
+    };
 
     matrix.prototype.scale = function (sx, sy, sz) {
         var sx = sx || 1;
         var sy = sy || 1;
         var sz = sz || 1;
-        var s = [ 
-                    sx, 0, 0, 0,
-                    0, sy, 0, 0,
-                    0, 0, sz, 0,
-                    0, 0, 0, 1
-                ];
+        var s = new Matrix([ 
+                                sx, 0, 0, 0,
+                                0, sy, 0, 0,
+                                0, 0, sz, 0,
+                                0, 0, 0, 1
+                            ], 4, 4);
         return s;
-    }
+    };
 
-    matrix.prototype.rotation = function (theta, x, y, z) {
+    matrix.prototype.rotate = function (theta, x, y, z) {
         var x = x || 0;
         var y = y || 0;
         var z = z || 0;
         var theta = theta || 0;
         var cos = Math.cos(theta);
         var sin = Math.sin(theta);
-        var cosMinus = 1 - cos;
-        var r = [
-                    cos + Math.pow(x,2) * cosMinus,
-                    y * x * cosMinus + z * sin,
-                    z * x * cosMinus - y * sin,
-                    0,
-                    x * y * cosMinus - z * sin,
-                    cos + Math.pow(y, 2) * cosMinus,
-                    z * y * cosMinus + x * sin,
-                    0,
-                    x * z * cosMinus + y * sin,
-                    y * z * cosMinus - x * sin,
-                    cos + Math.pow(z, 2) * cosMinus,
-                    0,
-                    0, 0, 0, 1
-                ];
+        var oneMinusCos = 1 - cos;
+        var r = new Matrix([
+                                cos + Math.pow(x,2) * oneMinusCos,
+                                x * y * oneMinusCos - z * sin,
+                                x * z * oneMinusCos + y * sin,
+                                0,
+                                y * x * oneMinusCos + z * sin,
+                                cos + Math.pow(y, 2) * oneMinusCos,
+                                y * z * oneMinusCos - x * sin,
+                                0, 
+                                z * x * oneMinusCos - y * sin,
+                                z * y * oneMinusCos + x * sin,
+                                cos + Math.pow(z, 2) * oneMinusCos,
+                                0,
+                                0, 0, 0, 1
+                            ], 4, 4);
         return r;
-    }
+    };
 
     matrix.prototype.orthographicProjection = function (l, r, b, t, n, f) {
         var width = r - l;
@@ -113,8 +113,8 @@ var Matrix = (function () {
                     0, 0, -2 / depth, 0,
                     -(r + l) / width, -(t + b) / height, -(f + n) / depth, 1
                 ];
-        return p;
-    }
+        return matrix.prototype.multiply(p);
+    };
 
     matrix.prototype.perspectiveProjection = function (l, r, b, t, n, f) {
         var width = r - l;
@@ -126,8 +126,13 @@ var Matrix = (function () {
                     (r + l) / width, (t + b) / height, -(f + n) / depth, -1,
                     0, 0, -2 * n * f / depth, 0,
                 ];
-        return p;
-    }
+        return matrix.prototype.multiply(p);
+    };
+
+    matrix.prototype.convert = function () {
+        var result = new Float32Array();
+        return result;
+    };
 
     return matrix;
 })();
